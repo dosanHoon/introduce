3cf7bf52b810259bb2f8ccfcc3f11032a67ab60b
한승훈 <hangru1106@naver.com>
2020-12-06 19:45:56 +0900

    휠 액션으로 마우스 페이지 넘기는 컴포넌트

--- src/Components/Pages.tsx
+++ src/Components/Pages.tsx
@@ -6,6 +6,35 @@ interface PropsTypes {
 }
 export const Pages: React.FC = ({ children }) => {
   const [p, setP] = React.useState(0);
+
+  const nextPage = () =>
+    setP((prevState) => {
+      const nextP = prevState + 1;
+      if (nextP < (children as []).length) return nextP;
+      return prevState;
+    });
+
+  const prevPage = () =>
+    setP((prevState) => {
+      const prevP = prevState - 1;
+      if (prevP < 0) return 0;
+      return prevP;
+    });
+
+  const wheelHandler = (event: any) => {
+    if (event.deltaY > 0) {
+      nextPage();
+    } else {
+      prevPage();
+    }
+  };
+  React.useEffect(() => {
+    window.addEventListener("wheel", wheelHandler);
+    return function () {
+      window.removeEventListener("wheel", wheelHandler);
+    };
+  }, []);
+  console.log("p", p);
   return (children as any)[p];
 };
 
